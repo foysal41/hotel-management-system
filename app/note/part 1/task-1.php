127829c13db866cfec8ec89aec3c9b574d953d0f
foysal41 <mdfoysal.mf33@gmail.com>
2024-12-16 02:19:45 +0600

    create controller, migration, model of hotel

---
+++ app/note/part 1/task-1.php
@@ -0,0 +1,11 @@
+<?php
+
+/*     Part 1  */
+
+/*
+    1. Create create hotel controller also  Hotel, room, and migrate to database
+    2. Routes -> make post, get, put, delete routes
+
+
+*/
+
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HotelController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::post('storehotel', [HotelController::class, 'store']);
+Route::get('allhotels' , [HotelController::class, 'index']);
+Route::put('uploadhotel/{id}' , [HotelController::class , 'update']);
+Route::delete('deletehotel/{id}' , [HotelController::class , 'destroy']);
